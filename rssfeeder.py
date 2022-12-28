8968e984308db52fdc15baeb2ff7da308f7d33dd
g.hidalgo-gadea@web.de <66069491+Guillermo-Hidalgo-Gadea@users.noreply.github.com>
2022-12-28 10:07:22 +0100

    added file import and xml as html landing page

--- rssfeeder.py
+++ rssfeeder.py
@@ -11,45 +11,72 @@ password = config['password']
 directory = config['directory']
 source = config['source']
 
-# def xml parts
+# prepare xml parts
+def xmlparser(title, link, description):
+    start = f'''
+<?xml version="1.0" encoding="UTF-8" ?>
+<rss version="2.0">
+<channel>
+  <title>{title}</title>
+  <link>{link}</link>
+  <description>{description}</description>
+'''
+    end = '''
+</channel>
+</rss>
+    '''
+    return start, end
+
+def xmlitem(title, link, description):
+    item = f'''
+    <item>
+        <title>{title}</title>
+        <link>{link}</link>
+        <description>{description}</description>
+    </item>'''
+    return item
+
+def getinfo(item):
+    title = (' ').join(os.path.basename(item).split('.')[0].split('_'))
+    link = 'https://'+item
+    # TODO get more description from file
+    description = f'This is a cool audiobook titled: {title}, enjoy!' 
+    return title, link, description
 
 # start server connection
 ftp = FTP(server)
 ftp.login(username, password)
 
-# list files on server
-files = ftp.nlst(directory)
-audiobooks = [file for file in files if 'mp3' in file]
-
 # import new audiobooks from source
+uploadcue = [os.path.join(source, file) for file in os.listdir(source) if 'mp3' in file]
 
-# write xml on server
+for file in uploadcue:
+    print(f'Uploading file "{os.path.basename(("_").join(file.split()))}" to ftp://{directory} ...')
+    with open(file,'rb') as book:
+        ftp.storbinary(f'STOR {os.path.join(directory, os.path.basename(("_").join(file.split())))}', book)
+
+# list files on server
+audiobooks = [file for file in ftp.nlst(directory) if 'mp3' in file]
+
+# prepare xml
+title = 'My self-hosted Audiobooks'
+link = 'https://GuillermoHidalgoGadea.com'
+description = 'RSS feed to my self-hosted Audiobooks'
+
+start, end = xmlparser(title, link, description)
+
+# write rss feed as xml
 with open('RSS.xml', 'w') as f:
-    f.write('''<?xml version="1.0" encoding="UTF-8" ?>
-<rss version="2.0">
-<channel>
-  <title>My self-hosted Audiobooks</title>
-  <link>https://GuillermoHidalgoGadea.com</link>
-  <description>RSS feed to my self-hosted Audiobooks</description>
-''')
+    f.write(start)
 
     for item in audiobooks:
-        title = os.path.basename(item)
-        link = 'https://'+item
-        description = 'This is a cool audiobook'
-
-        f.write(f'''<item>
-    <title>{title}</title>
-    <link>{link}</link>
-    <description>{description}</description>
-    </item>
-    ''')
-    f.write('''</channel>
-</rss>
-    ''')
+        title, link, description = getinfo(item)
+        f.write(xmlitem(title, link, description))
+    
+    f.write(end)
 
 # upload feed and quit connection
 with open('RSS.xml','rb') as feed:
-    ftp.storbinary(f'STOR {directory}/RSS.xml', feed)
+    ftp.storbinary(f'STOR {directory}/index.html', feed)
 
 ftp.quit()
