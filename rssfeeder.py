93e19c01cd6760d3e7f4c051593baa5b841b7add
g.hidalgo-gadea@web.de <66069491+Guillermo-Hidalgo-Gadea@users.noreply.github.com>
2023-11-12 14:33:51 +0100

    update

--- rssfeeder.py
+++ rssfeeder.py
@@ -1,10 +1,9 @@
 # Generate an RSS feed from a list of files
 
-import os, toml
+import os, toml, time
 from ftplib import FTP
 
 # read config
-
 config = toml.load(os.path.join(os.path.dirname(os.path.realpath(__file__)), 'config.toml'))
 server = config['server']
 username = config['username']
@@ -14,25 +13,31 @@ source = config['source']
 
 # prepare xml parts
 def xmlparser(title, link, description):
-    start = f'''
-<?xml version="1.0" encoding="UTF-8" ?>
-<rss version="2.0">
+    start = f'''<?xml version="1.0" encoding="UTF-8" ?>
+<rss version="2.0" xmlns:itunes="http://www.itunes.com/dtds/podcast-1.0.dtd"  xmlns:content="http://purl.org/rss/1.0/modules/content/">
 <channel>
-  <title>{title}</title>
-  <link>{link}</link>
-  <description>{description}</description>
-'''
+    <title>{title}</title>
+    <link>{link}</link>
+    <description>{description}</description>
+    <language>"en-us"</language>
+    <image>
+	    <url>https://rss.guillermohidalgogadea.com/logo.png</url>
+	    <title>Logo</title>
+	    <link>https://GuillermoHidalgoGadea.com</link>
+    </image>'''
     end = '''
 </channel>
 </rss>
     '''
     return start, end
 
-def xmlitem(title, link, description):
+def xmlitem(title, link, description, size, date):
     item = f'''
     <item>
         <title>{title}</title>
+        <enclosure url = "{link}" length="{size}" type="video/mpeg" />
         <link>{link}</link>
+        <pubDate>{date} GMT</pubDate>
         <description>{description}</description>
     </item>'''
     return item
@@ -42,11 +47,14 @@ def getinfo(item):
     link = 'https://'+item
     # TODO get more description from file
     description = f'This is a cool audiobook titled: {title}, enjoy!' 
-    return title, link, description
+    size = ftp.size(item)
+    return title, link, description, size
 
 # start server connection
 ftp = FTP(server)
-ftp.login(username, password)
+print(ftp.getwelcome())
+loginRepsonse = ftp.login(username, password)
+print(loginRepsonse)
 
 # import new audiobooks from source
 uploadcue = [os.path.join(source, file) for file in os.listdir(source) if 'mp3' in file]
@@ -57,28 +65,48 @@ for file in uploadcue:
         ftp.storbinary(f'STOR {os.path.join(directory, os.path.basename(("_").join(file.split())))}', book)
 
 # list files on server
+print("reading server content...")
 audiobooks = [file for file in ftp.nlst(directory) if 'mp3' in file]
+print(f"Found {len(audiobooks)} audiobooks!")
+ftp.sendcmd("TYPE i")
 
 # prepare xml
-title = 'My self-hosted Audiobooks'
+title = 'My Audiobooks'
 link = 'https://GuillermoHidalgoGadea.com'
 description = 'RSS feed to my self-hosted Audiobooks'
-
+# TODO get creation date instead
+date = time.strftime("%a, %d %b %Y %I:%M:%S", time.gmtime())
 start, end = xmlparser(title, link, description)
 
 # write rss feed as xml
+print("writing xml feed...")
 rss_file = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'RSS.xml')
 with open(rss_file, 'w') as f:
     f.write(start)
 
     for item in audiobooks:
-        title, link, description = getinfo(item)
-        f.write(xmlitem(title, link, description))
+        title, link, description, size = getinfo(item)
+        f.write(xmlitem(title, link, description, size, date))
+    
+    f.write(end)
+
+htmlfeed = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'index.html')
+with open(htmlfeed, 'w') as f:
+    f.write(start)
+
+    for item in audiobooks:
+        title, link, description, size = getinfo(item)
+        f.write(xmlitem(title, link, description, size, date))
     
     f.write(end)
 
 # upload feed and quit connection
-with open(rss_file,'rb') as feed:
+ftp.sendcmd("TYPE a")
+with open(htmlfeed,'rb') as feed:
     ftp.storbinary(f'STOR {directory}/index.html', feed)
 
 ftp.quit()
+print("RSS feed updated!")
+
+f = open(htmlfeed, 'r')
+print(f.read())
